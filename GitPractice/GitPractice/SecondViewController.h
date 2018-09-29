e52bfdb091f3108eda47aefbf031240c39ce9a68
zhaopy <zhaopeiyang1218@outlook.com>
2018-09-29 17:12:10 +0800

    B--创建SecondViewController

---
+++ GitPractice/GitPractice/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  GitPractice
+//
+//  Created by zhaopy on 2018/9/29.
+//  Copyright © 2018年 yidian-inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
