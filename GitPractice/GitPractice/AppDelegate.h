05a35f213a548d360491407b4cb51cc7c2e957f7
zhaopy <zhaopeiyang1218@outlook.com>
2018-09-29 17:06:29 +0800

    创建工程

---
+++ GitPractice/GitPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitPractice
+//
+//  Created by zhaopy on 2018/9/29.
+//  Copyright © 2018年 yidian-inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
